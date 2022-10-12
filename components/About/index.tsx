f4a074863797dd4b660c87492692eb3e53c87ce4
“aurovD” <dmitry8891@mail.ru>
2022-10-12 19:40:34 +0300

    about page first section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import styles from './About.module.scss';
 import clsx from "clsx";
+import Link from "next/link";
 
 export const About: React.FC = () => {
     return (
-        <div className={clsx("container", styles.about)}>
-            <div className={clsx(styles.introduction)}>
-                <h1>
-                    Aurov dmitry
-                </h1>
-                <div className={clsx(styles.introduction__main)}>
-                    <img className={clsx(styles.introduction__photo)} src="/assets/me.JPG" alt="avatar"/>
-                    <div className={clsx(styles.introduction__text)}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa dolor dolores et exercitationem explicabo fugiat libero nesciunt nisi numquam, obcaecati omnis praesentium quam quidem similique ullam unde vel voluptatibus. Beatae consectetur dignissimos facilis molestiae nemo. Commodi dolor dolore doloremque facilis fugit, magni odio quod tempora unde veritatis voluptatem voluptates.
+        <div className={clsx(styles.about)}>
+            <div className={clsx(styles.introduction__bg)}>
+                <div className={clsx(styles.introduction__bg_dark)}></div>
+            </div>
+            <div className={clsx("sticky", styles.introduction)}>
+                <div className={clsx(styles.introduction__content)}>
+                    <div className={clsx("mb-30", styles.introduction__header)}>
+                        <h3>ДМИТРИЙ</h3>
+                        <h3>АЮРОВ</h3>
+                        <h3>ВЕБ-РАЗРАБОТЧИК</h3>
+                    </div>
+                    <p>Занимаюсь разработкой несколько лет и готов реализовать Ваши идеи в осмысленное, изящное и красивое воплощение, которые привлечет клиентов и повысит конверсию. Для начала работы и сотрудничества  нам надо поговорить, так что кликайте на ссылку телеграмма внизу страницы</p>
+                    <div className={clsx("d-flex mt-30", styles.introduction__contacts)}>
+                        <Link  href={"https://t.me/aurovdm"}>
+                            TELEGRAM
+                        </Link>
+                        <Link  href={"https://github.com/AurovD"}>
+                            VKONTAKTE
+                        </Link>
+                        <Link  href={"/CV.pdf"}>
+                            LINKDEKIN
+                        </Link>
                     </div>
                 </div>
             </div>
-            jkhk
+            <div className={clsx("sticky", styles.education)}>
+                <h3>EDUCATION</h3>
+            </div>
         </div>
     )
 };
