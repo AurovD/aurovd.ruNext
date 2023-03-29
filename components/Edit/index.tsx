1577172f08c35910f9a8b1be2a936040727c46e1
“aurovD” <dmitry8891@mail.ru>
2023-03-29 23:34:29 +0300

    edit

---
+++ components/Edit/index.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import styles from "../Projects/Projects.module.scss";
+import ProjectCard from "../ProjectCard";
+import React from "react";
+
+export const Edit = () => {
+
+    return (
+        <div className={clsx("wrapper")}>
+            jgkgk
+        </div>
+    )
+};
--- pages/update/[update].tsx
+++ pages/update/[update].tsx
@@ -1,7 +1,12 @@
 import Panel from "../../components/Panel";
 import Head from "next/head";
+import {GetServerSideProps} from "next";
+import {ProjectsApi} from "../../api/ProjectsApi";
+import {Axios} from "../../axios/axios";
+import {Edit} from "../../components/Edit";
+import {getCookies} from "cookies-next";
 
-export default function UpdatePage() {
+export default function UpdatePage({project}) {
     const obj = {
         title: "РЕДАКТИРОВАНИЕ"
     }
@@ -9,9 +14,39 @@ export default function UpdatePage() {
         <div className={"d-grid grid"}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Aurov Dmitry - Portfolio</title>
+                <title>Редактирование</title>
             </Head>
             <Panel {...obj}/>
+            <Edit/>
         </div>
     )
 }
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    try {
+        const user = await ProjectsApi(Axios).checkAuth(getCookies(ctx).token);
+        if(!user){
+            return {
+                redirect: {
+                    permanent: false,
+                    destination: '/'
+                }
+            }
+        } else {
+            const projectId = ctx.query.update;
+            const project = await ProjectsApi(Axios).getProject(projectId as string);
+            return {
+                props: {
+                    project
+                },
+            }
+        }
+    } catch (error) {
+        return {
+            props: {},
+            redirect: {
+                destination: '/projects',
+                permanent: false
+            }
+        }
+    }
+};
