b1a517938b9ac8dcec81df1321b08a7b2eb6f6ea
“aurovD” <dmitry8891@mail.ru>
2023-08-07 16:23:20 +0300

    desc fixes

--- components/UI/ProjectDescription/index.tsx
+++ components/UI/ProjectDescription/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import clsx from "clsx";
 import styles from './ProjectDescription.module.scss';
+import sanitizeHtml from 'sanitize-html';
 
-export const ProjectDescription: React.FC<{description: string}> = ({description}) => {
+export const ProjectDescription: React.FC<{ description: string }> = ({ description }) => {
+    const sanitizeOptions = {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'h3', 'p']), // Add any additional allowed tags
+        allowedAttributes: {},
+    };
+
+    const sanitizedDescription = sanitizeHtml(description, sanitizeOptions);
+
     return (
-        <div className={clsx("border-box m-10", styles.description)}>
-            {description}
+        <div className={clsx('border-box m-10', styles.description)}>
+            <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
         </div>
-    )
+    );
 };
